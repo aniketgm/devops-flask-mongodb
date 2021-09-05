785947729caf02f8149777e910b3c4dc614a5aef
Aniket Meshram <aniket.g.meshram@gmail.com>
2021-09-05 20:00:28 +0530

    Modified config.py

--- App/config.py
+++ App/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     # SECRET_KEY = b'0\xfa\x1c|\xb0\x9a\xa5l\xc92\x10\xaf\xba\xfa\xb5\xfa'
-    SECRET_KEY = 'you-will-never-guess'
-    DB_HOST = 'localhost'
-    DB_PORT = 27017
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    DB_HOST = os.environ.get('DB_NAME') or 'localhost'
+    DB_PORT = os.environ.get('DB_PORT') or 27017
     DB_NAME = 'user_login'
